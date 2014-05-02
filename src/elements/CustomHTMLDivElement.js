7914ba6b71e8be3257418d0a5679475641922b58
Dimitri Glazkov <dglazkov@chromium.org>
2014-05-02 15:49:01 -0700

    Throw together some basic plumbing.
    Sketching out the structure of the repository.

---
+++ src/elements/CustomHTMLDivElement.js
@@ -0,0 +1,1 @@
+export class CustomHTMLDivElement extends HTMLElement {}
---
+++ src/html.js
@@ -0,0 +1,4 @@
+import {CustomHTMLDivElement} from './elements/CustomHTMLDivElement';
+
+// Register elements
+document.registerElement('custom-div', CustomHTMLDivElement);
