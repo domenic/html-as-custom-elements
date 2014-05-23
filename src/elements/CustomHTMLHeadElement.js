31fd51527da002a386664c03bb95e6f222ad114f
Erik Arvidsson <arv@chromium.org>
2014-05-23 16:17:18 -0700

    Add head and HTMLHeadElement

---
+++ src/elements/CustomHTMLHeadElement.js
@@ -0,0 +1,6 @@
+// http://www.whatwg.org/specs/web-apps/current-work/#the-head-element
+// http://www.whatwg.org/specs/web-apps/current-work/#htmlheadelement
+
+class CustomHTMLHeadElement extends HTMLElement {}
+
+export default CustomHTMLHeadElement;
--- test/all.js
+++ test/all.js
@@ -1,3 +1,4 @@
 import './blockquote';
 import './div';
+import './head';
 import './span';
