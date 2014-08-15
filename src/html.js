83fb2f9e5fda075307485417e071d41d4fa0f006
Domenic Denicola <domenic@domenicdenicola.com>
2014-08-15 17:58:11 -0400

    Add CustomHTMLHRElement (part of #33)

--- src/html.js
+++ src/html.js
@@ -1,10 +1,11 @@
 import CustomHTMLDivElement from './elements/CustomHTMLDivElement';
 import CustomHTMLHeadElement from './elements/CustomHTMLHeadElement';
+import CustomHTMLHRElement from './elements/CustomHTMLHRElement';
 import CustomHTMLQuoteElement from './elements/CustomHTMLQuoteElement';
 import CustomHTMLSpanElement from './elements/CustomHTMLSpanElement';
 
-// Register elements
 document.registerElement('custom-blockquote', CustomHTMLQuoteElement);
 document.registerElement('custom-div', CustomHTMLDivElement);
 document.registerElement('custom-head', CustomHTMLHeadElement);
+document.registerElement('custom-hr', CustomHTMLHRElement);
 document.registerElement('custom-span', CustomHTMLSpanElement);
