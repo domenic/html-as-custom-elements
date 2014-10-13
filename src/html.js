03e46915ed9712498f20154f5a725ab25229ce0a
Domenic Denicola <domenic@domenicdenicola.com>
2014-10-13 18:28:41 -0400

    Add skeleton work for HTMLAudioElement
    This currently just contains a lot of mostly-working auto-generated stubs for the HTMLMediaElement interface. Recent upgrades to webidl-class-generator and webidl-conversions have enabled that to generate a lot better, but it still doesn't *do* anything. We need to implement the delegation of the generated class to the implementation class next, and this gives us a playground in which to do it.

--- src/html.js
+++ src/html.js
@@ -4,8 +4,13 @@ import CustomHTMLHRElement from './elements/CustomHTMLHRElement';
 import CustomHTMLQuoteElement from './elements/CustomHTMLQuoteElement';
 import CustomHTMLSpanElement from './elements/CustomHTMLSpanElement';
 
+import './elements/CustomHTMLMediaElement';
+import CustomHTMLAudioElement from './elements/CustomHTMLAudioElement';
+
 document.registerElement('custom-blockquote', CustomHTMLQuoteElement);
 document.registerElement('custom-div', CustomHTMLDivElement);
 document.registerElement('custom-head', CustomHTMLHeadElement);
 document.registerElement('custom-hr', CustomHTMLHRElement);
 document.registerElement('custom-span', CustomHTMLSpanElement);
+
+document.registerElement('custom-audio', CustomHTMLAudioElement);
